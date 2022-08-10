aac5eeabac17c43a6bfbec1763e048ad9c549aaf
Evan Nouis <evan.nouis@gmail.com>
2022-08-09 20:09:15 -0500

    imported BrowserRouter into index.js and Link into App.js. Invoices and Expenses links working.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export default function App() {
   return (
     <div>
       <h1>Bookkeeper!</h1>
+      <nav >
+        <Link to="/invoices" >Invoices</Link>
+        <Link to="/expenses" >Expenses</Link>
+      </nav>
     </div>
   );
 }
