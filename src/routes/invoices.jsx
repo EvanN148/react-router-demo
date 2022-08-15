cd18085b20b2abaa2976e6478852262b6a951b1a
EvanN148 <evan.nouis@gmail.com>
2022-08-14 22:28:06 -0500

    Successfully linking invoice data and assigning corresponding url in Invoices component.

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,13 +1,20 @@
-import React from  'react'
+import { Link } from 'react-router-dom'
+
 import { getInvoices }from '../data'
 
-export default function Invoices(props) {
+export default function Invoices() {
+  let invoices = getInvoices()
   return (
     <h1 >
       Invoices
-      {props.data.map(point => {
+      {invoices.map(invoice => {
         return (
-          <div >{point}</div>
+          <Link 
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
         )
       })}
     </h1>
